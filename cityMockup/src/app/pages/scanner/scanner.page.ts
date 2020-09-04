0e6ad8a1ec9f0e186973ce4384ef50c38c77e66a
kkuczynski <krzysiek.kuczynski@icloud.com>
2020-09-04 13:30:08 +0200

    added qr scanner

--- cityMockup/src/app/pages/scanner/scanner.page.ts
+++ cityMockup/src/app/pages/scanner/scanner.page.ts
@@ -1,50 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { QRScanner } from 'cordova-plugin-qrscanner'
-
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
 @Component({
   selector: 'page-scanner',
   templateUrl: './scanner.page.html',
   styleUrls: ['./scanner.page.scss'],
 })
 export class ScannerPage implements OnInit {
-//  public qrScanner = window.QRScanner;
-  constructor() { }
+  //  public qrScanner = window.QRScanner;
+  constructor(private qrScanner: QRScanner) { }
 
   ngOnInit() {
-    // this.qrScanner.prepare(this.onDone); // show the prompt
+    // Optionally request the permission early
+    this.qrScanner.prepare()
+      .then((status: QRScannerStatus) => {
+        if (status.authorized) {
+          // camera permission was granted
+
+
+          // start scanning
+          let scanSub = this.qrScanner.scan().subscribe((text: string) => {
+            console.log('Scanned something', text);
+
+            this.qrScanner.hide(); // hide camera preview
+            scanSub.unsubscribe(); // stop scanning
+          });
+
+        } else if (status.denied) {
+          // camera permission was permanently denied
+          // you must use QRScanner.openSettings() method to guide the user to the settings page
+          // then they can grant the permission from there
+        } else {
+          // permission was denied, but not permanently. You can ask for permission again at a later time.
+        }
+      })
+      .catch((e: any) => console.log('Error is', e));
+
   }
 
-  // onDone(err, status) {
-  //   if (err) {
-  //     // here we can handle errors and clean up any loose ends.
-  //     console.error(err);
-  //   }
-  //   if (status.authorized) {
-  //     // W00t, you have camera access and the scanner is initialized.
-  //     // QRscanner.show() should feel very fast.
-  //     QRScanner.scan(this.displayContents);
-  //     // Make the webview transparent so the video preview is visible behind it.
-  //     QRScanner.show();
-  //     // Be sure to make any opaque HTML elements transparent here to avoid
-  //     // covering the video.
-  //   } else if (status.denied) {
-  //     // The video preview will remain black, and scanning is disabled. We can
-  //     // try to ask the user to change their mind, but we'll have to send them
-  //     // to their device settings with `QRScanner.openSettings()`.
-  //   } else {
-  //     // we didn't get permission, but we didn't get permanently denied. (On
-  //     // Android, a denial isn't permanent unless the user checks the "Don't
-  //     // ask again" box.) We can ask again at the next relevant opportunity.
-  //   }
-  // }
-  // displayContents(err, text) {
-  //   if (err) {
-  //     // an error occurred, or the scan was canceled (error code `6`)
-  //   } else {
-  //     // The scan completed, display the contents of the QR code:
-  //     alert(text);
-  //   }
-  // }
 
 
 }
